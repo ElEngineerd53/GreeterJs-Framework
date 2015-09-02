26c6dd13e7e2735513436ef94db5f8641b38c0e9
ayacaman <ayacaman@outlook.com>
2015-09-01 21:12:49 -0700

    Basic Set up for the Object and a Prototype.

--- Greetr.js
+++ Greetr.js
@@ -12,5 +12,27 @@
  */
  
  (function(global,$){
+	 var Greetr = function(firstName, lastName, language) {
+		 // Return function constructor so that the user does
+		 // not have to declare the new keyword.
+		 return new Greetr.init(firstName,lastName,language);
+	 }
+	 // This is where we will store Properties & Methods that we want to use
+	 // inside the object that is returned from Greetr. 
+	 Greetr.prototype = {};
 	 
+	 // This is the Greetr function.
+	 Greetr.init = function(firstName, lastName, language) {
+		 // Check to see if firstName & lastName are initialize,
+		 // and if not set them equal to an empty string.
+		 // For language set it equal to english if no language was passed.
+		 var self = this;
+		 self.firstName = firstName || '';
+		 self.lastName = lastName || '';
+		 self.language = language || 'en';
+	 }
+	 // Here is where the objects will point to. 
+	 Greetr.init.prototype = Greetr.prototype;
+	 // Exposing the Greetr function so the user can use 'G$()'.
+	 global.Greetr = global.G$ = Greetr;
  })(window,jQuery);
